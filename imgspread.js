d96df3735cb4290d83f82b5c570e293f12b47908
klml <m@klml.de>
2015-04-13 20:54:53 +0200

    jquery plugin thx to http://www.queness.com/post/112/a-really-simple-jquery-plugin-tutorial

--- imgspread.js
+++ imgspread.js
@@ -1,11 +1,20 @@
-jQuery(document).ready(function($) {
-    $('.imgspread').click( function ( event ) {
-        var img = $(this).find('img') ,
-            src = img.attr('src') ,
-            href = $(this).attr('href') ;
+(function($){
+     $.fn.extend({ 
 
-        img.attr('src', href ) ;
-        $(this).attr('href', src ) ;
-        event.preventDefault();
+         imgspread: function(options) {
+
+            return this.each(function() {
+
+                 $(this).click( function ( event ) {
+                    var img = $(this).find('img') ,
+                        src = img.attr('src') ,
+                        href = $(this).attr('href') ;
+            
+                    img.attr('src', href ) ;
+                    $(this).attr('href', src ) ;
+                    event.preventDefault();
+                });
+            });
+        }
     });
-});
+})(jQuery);
